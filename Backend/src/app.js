e680754894632594e7403df184201723623ac957
Ashish Prasad <33520299+theprogrammedwords@users.noreply.github.com>
2021-09-06 01:11:59 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,19 +6,19 @@ const cors = require("cors");
 const config = require("./config/config");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(config.mongoose.url).then(() => {
+
+  console.log("Connected to MongoDB");
+  app.listen(config.port, () => {
+    console.log(`App is running on port ${config.port}`);
+  });
+
+});
 
 // NOTE - Uncomment in Milestone 5
 // Middleware to log API request metadata
 // app.use(captureDateMiddleware);
-
 app.use("/v1", routes);
-
-// TODO - Create a MongoDB connection using Mongoose
-
-// Start the Node server
-app.listen(config.port, () => {
-  console.log(`App is running on port ${config.port}`);
-});
